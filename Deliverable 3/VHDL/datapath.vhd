9ba09612fc87324e78f301dae871a10147d8c8a6
SebastianPilarski <sebastian.pilarski@mail.mcgill.ca>
2017-02-06 18:58:34 -0500

    Datapath complete implements controller - still in separate file - not sure what to do with m_adr

--- Deliverable 3/VHDL/datapath.vhd
+++ Deliverable 3/VHDL/datapath.vhd
@@ -6,75 +6,132 @@ use lpm.lpm_components.all;
 
 entity datapath is
     port(
-        clk            : in  std_logic; -- TODO: check to make sure port I/O is correct
-        -- Avalon interface
-        s_read         : out  std_logic;
-        s_write        : out  std_logic;
-        m_waitrequest  : out  std_logic;
-        -- Cache logic interface 
-        tag_hit        : out  std_logic;
-        byte_done      : out  std_logic;
-        word_done      : out  std_logic;
-        -- Cache storage interface
-        valid          : out  std_logic;
-        dirty          : out  std_logic;
-        dirty_data     : out  std_logic;
-        -- Avalon interface
-        m_read         : in std_logic;
-        m_write        : in std_logic;
-        s_waitrequest  : in std_logic;
-        -- Cache storage interface
-        c_read         : in std_logic;
-        c_write        : in std_logic;
-        c_write_sel    : in std_logic;
-        c_write_reg_en : in std_logic;
-        c_dirty_clr    : in std_logic;
-        -- Cache Logic interface
-        tag_sel        : in std_logic;
-        word_sel       : in std_logic;
-        word_en        : in std_logic;
-        word_clr       : in std_logic;
-        byte_en        : in std_logic;
-        byte_clr       : in std_logic;
-		  
-		  s_adr			  : in std_logic_vector(31 downto 0));
-		  --s_read 		  : in std_logic;
+        clk            : in  std_logic;
+        s_read         : in  std_logic;
+        s_write        : in  std_logic;
+        m_waitrequest  : in  std_logic;
+        m_read         : out std_logic;
+        m_write        : out std_logic;
+        s_waitrequest  : out std_logic;
+		  s_adr			  	: in std_logic_vector(31 downto 0); --additional I/O needed for Cache
+		  s_readdata		: out std_logic_vector(31 downto 0);
+		  s_writedata		: in std_logic_vector(31 downto 0);
+		  m_adr				: out std_logic_vector(31 downto 0);
+		  m_readdata      : in std_logic_vector(7 downto 0);
+		  m_writedata     : out std_logic_vector(7 downto 0));
 end datapath;
 
 architecture a1 of datapath is
+---------------------------------------------------------
+--	Signals
+---------------------------------------------------------
 -- data_in related
 	signal en1, en2, en3, en4: std_logic;
 	signal reg1, reg2, reg3, reg4: std_logic_vector(7 downto 0);
-	signal m_readdata: std_logic_vector(7 downto 0);
-	signal regdata, s_writedata, data_in: std_logic_vector(31 downto 0); --data_in in component
+	signal regdata, data_in: std_logic_vector(31 downto 0); --data_in in component
 	signal byte_offset: std_logic_vector(1 downto 0);
 -- block related
 	signal word_cnt, byte_cnt, block_offset: std_logic_vector(1 downto 0);
-	signal m_adr: std_logic_vector(31 downto 0);
 	signal tag_out, tag: std_logic_vector(5 downto 0); --tag_out also used in data_out
 	signal block_index: std_logic_vector(4 downto 0);
+	signal tag_hit, byte_done, word_done: std_logic;
+	signal tag_sel, word_sel, word_en, word_clr, byte_en, byte_clr: std_logic;
 -- data_out related
 	signal data_out: std_logic_vector(131 downto 0);
-	signal m_writedata: std_logic_vector(7 downto 0);
-	signal s_readdata: std_logic_vector(31 downto 0);
+	signal s_readdataline: std_logic_vector(31 downto 0);
+	signal dirty, dirty_data, valid: std_logic;
 -- component related
 	signal read, write, dirty_clr: std_logic;
 	signal tag_in: std_logic_vector(5 downto 0);
 	signal block_index_in: std_logic_vector(4 downto 0);
 	signal block_offset_in: std_logic_vector(1 downto 0);
 	signal data_in_comp: std_logic_vector(31 downto 0);
+	signal c_read, c_write, c_write_sel, c_write_reg_en, c_dirty_clr: std_logic;
 
 	signal data_out_comp: std_logic_vector(131 downto 0);
 	signal tag_out_comp: std_logic_vector(5 downto 0);
 	signal valid_out: std_logic;
+-- Miscellanious connecting signals
+	signal clock: std_logic;
 	
--- to be organized later
-begin
+-------------------------------------------------------------
+--	Components
+------------------------------------------------------------
+	component cache_controller
+		port(clk            : in  std_logic;
+        -- Avalon interface
+        s_read         : in  std_logic;
+        s_write        : in  std_logic;
+        m_waitrequest  : in  std_logic;
+        -- Cache logic interface 
+        tag_hit        : in  std_logic;
+        byte_done      : in  std_logic;
+        word_done      : in  std_logic;
+        -- Cache storage interface
+        valid          : in  std_logic;
+        dirty          : in  std_logic;
+        dirty_data     : in  std_logic;
+        -- Avalon interface
+        m_read         : out std_logic;
+        m_write        : out std_logic;
+        s_waitrequest  : out std_logic;
+        -- Cache storage interface
+        c_read         : out std_logic;
+        c_write        : out std_logic;
+        c_write_sel    : out std_logic;
+        c_write_reg_en : out std_logic;
+        c_dirty_clr    : out std_logic;
+        -- Cache Logic interface
+        tag_sel        : out std_logic;
+        word_sel       : out std_logic;
+        word_en        : out std_logic;
+        word_clr       : out std_logic;
+        byte_en        : out std_logic;
+        byte_clr       : out std_logic);
+	end component;
+	
+	begin -- BEGIN
 -----------------------------------------------------------
---	Matching equivalent signals
+--	Matching signals and Components
 -----------------------------------------------------------
+	clock <= clk;
 	byte_offset <= byte_cnt;
 	block_index <= s_adr(8 downto 4);
+	s_readdata  <= s_readdataline;
+	
+	controller: cache_controller 
+		port map(
+			clk => clock,
+			-- Avalon interface
+			s_read => s_read,
+			s_write => s_write,
+			m_waitrequest => m_waitrequest,
+			-- Cache logic interface 
+			tag_hit => tag_hit,   
+			byte_done => byte_done,
+			word_done => word_done,
+			-- Cache storage interface
+			valid => valid,
+			dirty => dirty,
+			dirty_data => dirty_data,
+			-- Avalon interface
+			m_read => m_read,   
+			m_write => m_write,
+			s_waitrequest => s_waitrequest,
+			-- Cache storage interface
+			c_read => c_read,    
+			c_write => c_write,  
+			c_write_sel => c_write_sel,  
+			c_write_reg_en => c_write_reg_en,
+			c_dirty_clr => c_dirty_clr,  
+			-- Cache Logic interface
+			tag_sel => tag_sel,	
+			word_sel => word_sel, 
+			word_en => word_en,
+			word_clr => word_clr,			
+			byte_en => byte_en,   
+			byte_clr => byte_clr  
+		);
 
 ------------------------------------------------------------
 --	data_in related
@@ -177,17 +234,17 @@ begin
 		data_out(66) when "10",
 		data_out(99) when "11";
 		
-	with block_offset select s_readdata <= --s_readdata
+	with block_offset select s_readdataline <= --s_readdata
 		data_out(32 downto 1)    when "00",
 		data_out(65 downto 34)   when "01",
 		data_out(98 downto 67)   when "10",
 		data_out(131 downto 100) when "11";
 		
-	with byte_offset select m_writedata <= -- m_writedata
-		s_readdata(7 downto 0)   when "00",
-		s_readdata(15 downto 8)  when "01",
-		s_readdata(23 downto 16) when "10",
-		s_readdata(31 downto 24) when "11";
+	with byte_offset select m_writedata <= -- 
+		s_readdataline(7 downto 0)   when "00",
+		s_readdataline(15 downto 8)  when "01",
+		s_readdataline(23 downto 16) when "10",
+		s_readdataline(31 downto 24) when "11";
 	
 	------------------------------------------------------------------------
 	--	Component Related
@@ -245,9 +302,7 @@ begin
 			if(read = '1' or write = '1') then -- 'release' data upon read or write signal
 				data_out <= data_out_comp;
 				tag_out <= tag_in;
-				valid <= valid_out;
-				
-				
+				valid <= valid_out;		
 			end if;
 		end if;
 	end process;
