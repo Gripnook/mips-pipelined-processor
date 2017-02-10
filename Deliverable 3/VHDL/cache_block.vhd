65d1599dbea18f92b7a0f599e984c50ef5b8497e
Gripnook <XxAndreixX@hotmail.com>
2017-02-09 19:03:28 -0500

    Fixed cache module
    Cleaned up code.
    Fixed reset bugs.
    Fixed glitch pulses.
    Delayed read hits by one clock cycle for consistency with writes.

--- Deliverable 3/VHDL/cache_block.vhd
+++ Deliverable 3/VHDL/cache_block.vhd
@@ -4,10 +4,10 @@ use ieee.numeric_std.all;
 
 entity cache_block is
     port(
-        reset           : in std_logic;
         clock           : in  std_logic;
-        read            : in  std_logic;
-        write           : in  std_logic;
+        reset           : in  std_logic;
+        read_en         : in  std_logic;
+        write_en        : in  std_logic;
         data_in         : in  std_logic_vector(31 downto 0);
         tag_in          : in  std_logic_vector(5 downto 0);
         block_index_in  : in  std_logic_vector(4 downto 0);
@@ -16,99 +16,54 @@ entity cache_block is
         data_out        : out std_logic_vector(131 downto 0);
         tag_out         : out std_logic_vector(5 downto 0);
         valid_out       : out std_logic);
-    
 end cache_block;
 
 architecture a1 of cache_block is
-    TYPE MEM IS ARRAY (31 downto 0) OF STD_LOGIC_VECTOR(138 DOWNTO 0); --128 for 4 words + 4 dirty bit + tag + 1 valid bit
-        -- format follows
-        --138 valid | 137-132 tag 
-        
-        --131-100 data offset 11, dirty 99
-        --98-67 data offset 10, dirty 66 
-        --65-34 data offset 01, dirty 33
-        --32-1 data offset 00, dirty 0
-    SIGNAL ram_block              : MEM := (others => (others => '0'));
-     
-         -- debug signals for ease of visibility
-     TYPE valid is array(31 downto 0) of std_logic;
-     signal debug_valid_array      : valid := (others => '0'); -- for ease of debugging
-     type dirty is array(31 downto 0) of std_logic_vector(3 downto 0);
-     signal debug_dirty              : dirty := (others => (others => '0'));
-     type tag is array(31 downto 0) of std_logic_vector(5 downto 0);
-     signal debug_tag              : tag   := (others => (others => '0'));
-     
-     begin
-     -- debug signals
-     debug_valid_array(to_integer(unsigned(block_index_in))) <= ram_block((to_integer(unsigned(block_index_in))))(138);
-     debug_dirty(to_integer(unsigned(block_index_in)))(0)    <= ram_block((to_integer(unsigned(block_index_in))))(0);
-     debug_dirty(to_integer(unsigned(block_index_in)))(1)    <= ram_block((to_integer(unsigned(block_index_in))))(33);
-     debug_dirty(to_integer(unsigned(block_index_in)))(2)    <= ram_block((to_integer(unsigned(block_index_in))))(66);
-     debug_dirty(to_integer(unsigned(block_index_in)))(3)    <= ram_block((to_integer(unsigned(block_index_in))))(99);
-     debug_tag(to_integer(unsigned(block_index_in)))         <= ram_block((to_integer(unsigned(block_index_in))))(137 downto 132);
-     
-    process(clock) -- component in data_path
-    begin
-        if(clock'event and clock = '1') then
-            if(reset = '1') then
-                ram_block <= (others => (others => '0'));
-            end if;
-        
-            if(read = '1') then
-                valid_out <= ram_block((to_integer(unsigned(block_index_in))))(138); --valid bit
-                tag_out <= ram_block((to_integer(unsigned(block_index_in))))(137 downto 132); -- tag
-                data_out <= ram_block((to_integer(unsigned(block_index_in))))(131 downto 0); -- data
-            end if;
-                    
+    type data_mem_type is array(0 to 127) of std_logic_vector(31 downto 0);
+    type dirty_mem_type is array(0 to 127) of std_logic;
+    type tag_mem_type is array(0 to 31) of std_logic_vector(5 downto 0);
+    type valid_mem_type is array(0 to 31) of std_logic;
 
-            
-            if(write = '1') then 
-                ram_block((to_integer(unsigned(block_index_in))))(138) <= '1'; --valid
-                if(block_offset_in = "00") then
-                    if(dirty_clr = '1') then
-                        ram_block((to_integer(unsigned(block_index_in))))(0) <= '0'; -- dirty bit offset 00
-                    elsif(dirty_clr = '0') then
-                        ram_block((to_integer(unsigned(block_index_in))))(0) <= '1'; --dirty bit offset 00
-                    end if;
-                        ram_block((to_integer(unsigned(block_index_in))))(32 downto 1) <= data_in; -- data offset 00
-                elsif(block_offset_in = "01") then
-                    if(dirty_clr = '1') then
-                        ram_block((to_integer(unsigned(block_index_in))))(33) <= '0'; -- dirty bit offset 01
-                    elsif(dirty_clr = '0') then
-                        ram_block((to_integer(unsigned(block_index_in))))(33) <= '1'; -- dirty bit offset 01
-                    end if;
-                        ram_block((to_integer(unsigned(block_index_in))))(65 downto 34) <= data_in; -- data offset 01
-                elsif(block_offset_in = "10") then
-                    if(dirty_clr = '1') then
-                        ram_block((to_integer(unsigned(block_index_in))))(66) <= '0'; -- dirty bit offset 10
-                    elsif(dirty_clr = '0') then
-                        ram_block((to_integer(unsigned(block_index_in))))(66) <= '1'; -- dirty bit offset 10
-                    end if;
-                        ram_block((to_integer(unsigned(block_index_in))))(98 downto 67) <= data_in; -- data offset 10
-                elsif(block_offset_in = "11") then
-                    if(dirty_clr = '1') then
-                        ram_block((to_integer(unsigned(block_index_in))))(99) <= '0'; -- dirty bit offset 11
-                    elsif(dirty_clr = '0') then
-                        ram_block((to_integer(unsigned(block_index_in))))(99) <= '1'; -- -- dirty bit offset 0
-                    end if;
-                        ram_block((to_integer(unsigned(block_index_in))))(131 downto 100) <= data_in; -- data offset 11
+    signal data_mem : data_mem_type;
+    signal dirty_mem : dirty_mem_type;
+    signal tag_mem : tag_mem_type;
+    signal valid_mem : valid_mem_type := (others => '0');
+
+begin
+    process(clock, reset)
+        variable block_addr, read_word_addr, write_word_addr : integer;
+    begin
+        if (reset = '1') then
+            valid_mem <= (others => '0');
+        elsif (rising_edge(clock)) then
+            block_addr := to_integer(unsigned(block_index_in));
+            read_word_addr := to_integer(unsigned(std_logic_vector'(block_index_in & "00")));
+            write_word_addr := to_integer(unsigned(std_logic_vector'(block_index_in & block_offset_in)));
+
+            if (read_en = '1') then
+                data_out <= data_mem(read_word_addr + 3) & dirty_mem(read_word_addr + 3) &
+                            data_mem(read_word_addr + 2) & dirty_mem(read_word_addr + 2) &
+                            data_mem(read_word_addr + 1) & dirty_mem(read_word_addr + 1) &
+                            data_mem(read_word_addr) & dirty_mem(read_word_addr);
+                tag_out <= tag_mem(block_addr);
+                valid_out <= valid_mem(block_addr);
+            end if;
+
+            if (write_en = '1') then 
+                valid_mem(block_addr) <= '1';
+                tag_mem(block_addr) <= tag_in;
+                data_mem(write_word_addr) <= data_in;
+                if (dirty_clr = '1') then
+                    dirty_mem(write_word_addr) <= '0';
+                else
+                    dirty_mem(write_word_addr) <= '1';
                 end if;
             end if;
             
-            if(dirty_clr = '1') then
-                if(block_offset_in = "00") then
-                    ram_block((to_integer(unsigned(block_index_in))))(0) <= '0'; -- dirty bit offset 00
-                elsif(block_offset_in = "01") then
-                    ram_block((to_integer(unsigned(block_index_in))))(33) <= '0'; -- dirty bit offset 01
-                elsif(block_offset_in = "10") then
-                    ram_block((to_integer(unsigned(block_index_in))))(66) <= '0'; -- dirty bit offset 10
-                elsif(block_offset_in = "11") then
-                    ram_block((to_integer(unsigned(block_index_in))))(99) <= '0'; -- dirty bit offset 11
-                end if;
+            if (dirty_clr = '1') then
+                dirty_mem(write_word_addr) <= '0';
             end if;
         end if;
-     
-     
     end process;
 
 end a1;
--- Deliverable 3/VHDL/cache_controller.vhd
+++ Deliverable 3/VHDL/cache_controller.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 entity cache_controller is
     port(
-        clk            : in  std_logic;
-        rst            : in  std_logic;
+        clock            : in  std_logic;
+        reset            : in  std_logic;
         -- Avalon interface
         s_read         : in  std_logic;
         s_write        : in  std_logic;
@@ -41,145 +41,11 @@ architecture arch of cache_controller is
     type state_type is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11);
     signal state : state_type := S0;
 begin
-    output_process : process(s_read, s_write, m_waitrequest, tag_hit, byte_done, word_done, valid, dirty, dirty_data, state)
+    state_transition_process : process(clock, reset)
     begin
-        -- Default outputs
-        m_read         <= '0';
-        m_write        <= '0';
-        s_waitrequest  <= '0';
-        c_read         <= '0';
-        c_write        <= '0';
-        c_write_sel    <= '0';
-        c_write_reg_en <= '0';
-        c_dirty_clr    <= '0';
-        tag_sel        <= '0';
-        word_sel       <= '0';
-        word_en        <= '0';
-        word_clr       <= '0';
-        byte_en        <= '0';
-        byte_clr       <= '0';
-
-        case state is
-            when S0 =>
-                if s_read = '1' or s_write = '1' then
-                    s_waitrequest <= '1';
-                    c_read        <= '1';
-                end if;
-            when S1 =>
-                if valid = '0' then
-                    s_waitrequest <= '1';
-                    word_sel      <= '1';
-                    m_read        <= '1';
-                else
-                    if tag_hit = '0' then
-                        s_waitrequest <= '1';
-                        word_sel      <= '1';
-                        if dirty = '0' then
-                            m_read <= '1';
-                        else
-                            tag_sel <= '1';
-                            if dirty_data = '0' then
-                                word_en <= '1';
-                            else
-                                m_write <= '1';
-                            end if;
-                        end if;
-                    else
-                        if s_read = '0' then
-                            s_waitrequest <= '1';
-                            c_write       <= '1';
-                            c_write_sel   <= '1';
-                        end if;
-                    end if;
-                end if;
-            when S2 =>
-            when S3 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                if m_waitrequest = '1' then
-                    m_read <= '1';
-                else
-                    c_write_reg_en <= '1';
-                    if byte_done = '0' then
-                        byte_en <= '1';
-                    end if;
-                end if;
-            when S4 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                m_read        <= '1';
-            when S5 =>
-                s_waitrequest <= '1';
-                c_write       <= '1';
-                byte_clr      <= '1';
-                word_sel      <= '1';
-                word_en       <= '1';
-                c_dirty_clr   <= '1';
-            when S6 =>
-                s_waitrequest <= '1';
-                c_write       <= '1';
-                word_sel      <= '1';
-                word_clr      <= '1';
-                byte_clr      <= '1';
-                c_dirty_clr   <= '1';
-            when S7 =>
-                s_waitrequest <= '1';
-                if s_read = '1' then
-                    c_read <= '1';
-                else
-                    c_write     <= '1';
-                    c_write_sel <= '1';
-                end if;
-            when S8 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                tag_sel       <= '1';
-                if m_waitrequest = '1' then
-                    m_write <= '1';
-                else
-                    if byte_done = '0' then
-                        byte_en <= '1';
-                    else
-                        byte_clr    <= '1';
-                        c_dirty_clr <= '1';
-                        if word_done = '0' then
-                            word_en <= '1';
-                        else
-                            word_clr <= '1';
-                        end if;
-                    end if;
-                end if;
-            when S9 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                tag_sel       <= '1';
-                m_write       <= '1';
-            when S10 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                tag_sel       <= '1';
-                if dirty_data = '1' then
-                    m_write <= '1';
-                else
-                    if word_done = '1' then
-                        byte_clr <= '1';
-                        word_clr <= '1';
-                    else
-                        word_en <= '1';
-                    end if;
-                end if;
-            when S11 =>
-                s_waitrequest <= '1';
-                word_sel      <= '1';
-                m_read        <= '1';
-        end case;
-    end process;
-
-    state_transition_process : process(clk, rst)
-    begin
-        if (rst = '1') then
+        if (reset = '1') then
             state <= S0;
-        elsif rising_edge(clk) then
+        elsif rising_edge(clock) then
             case state is
                 when S0 =>
                     if s_read = '1' or s_write = '1' then
@@ -202,11 +68,7 @@ begin
                                 end if;
                             end if;
                         else
-                            if s_read = '1' then
-                                state <= S0;
-                            else
-                                state <= S2;
-                            end if;
+                            state <= S2;
                         end if;
                     end if;
                 when S2 =>
@@ -263,5 +125,137 @@ begin
                     state <= S3;
             end case;
         end if;
+    end process;
+
+    output_process : process(s_read, s_write, m_waitrequest, tag_hit, byte_done, word_done, valid, dirty, dirty_data, state)
+    begin
+        -- Default outputs
+        m_read         <= '0';
+        m_write        <= '0';
+        s_waitrequest  <= '0';
+        c_read         <= '0';
+        c_write        <= '0';
+        c_write_sel    <= '0';
+        c_write_reg_en <= '0';
+        c_dirty_clr    <= '0';
+        tag_sel        <= '0';
+        word_sel       <= '0';
+        word_en        <= '0';
+        word_clr       <= '0';
+        byte_en        <= '0';
+        byte_clr       <= '0';
+
+        case state is
+            when S0 =>
+                if s_read = '1' or s_write = '1' then
+                    s_waitrequest <= '1';
+                    c_read        <= '1';
+                end if;
+            when S1 =>
+                s_waitrequest <= '1';
+                if valid = '1' then
+                    if tag_hit = '1' then
+                        if s_write = '1' then
+                            c_write       <= '1';
+                            c_write_sel   <= '1';
+                        end if;
+                    else
+                        word_sel      <= '1';
+                        if dirty = '1' then
+                            tag_sel <= '1';
+                            if dirty_data = '1' then
+                                m_write <= '1';
+                            else
+                                word_en <= '1';
+                            end if;
+                        else
+                            m_read <= '1';
+                        end if;
+                    end if;
+                else
+                    word_sel <= '1';
+                    m_read   <= '1';
+                end if;
+            when S2 =>
+            when S3 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                if m_waitrequest = '1' then
+                    m_read <= '1';
+                else
+                    c_write_reg_en <= '1';
+                    if byte_done = '0' then
+                        byte_en <= '1';
+                    end if;
+                end if;
+            when S4 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                m_read        <= '1';
+            when S5 =>
+                s_waitrequest <= '1';
+                c_write       <= '1';
+                byte_clr      <= '1';
+                word_sel      <= '1';
+                word_en       <= '1';
+                c_dirty_clr   <= '1';
+            when S6 =>
+                s_waitrequest <= '1';
+                c_write       <= '1';
+                word_sel      <= '1';
+                word_clr      <= '1';
+                byte_clr      <= '1';
+                c_dirty_clr   <= '1';
+            when S7 =>
+                s_waitrequest <= '1';
+                if s_read = '1' then
+                    c_read <= '1';
+                else
+                    c_write     <= '1';
+                    c_write_sel <= '1';
+                end if;
+            when S8 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                tag_sel       <= '1';
+                if m_waitrequest = '1' then
+                    m_write <= '1';
+                else
+                    if byte_done = '1' then
+                        byte_clr    <= '1';
+                        c_dirty_clr <= '1';
+                        if word_done = '1' then
+                            word_clr <= '1';
+                        else
+                            word_en <= '1';
+                        end if;
+                    else
+                        byte_en <= '1';
+                    end if;
+                end if;
+            when S9 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                tag_sel       <= '1';
+                m_write       <= '1';
+            when S10 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                tag_sel       <= '1';
+                if dirty_data = '1' then
+                    m_write <= '1';
+                else
+                    if word_done = '1' then
+                        byte_clr <= '1';
+                        word_clr <= '1';
+                    else
+                        word_en <= '1';
+                    end if;
+                end if;
+            when S11 =>
+                s_waitrequest <= '1';
+                word_sel      <= '1';
+                m_read        <= '1';
+        end case;
     end process;
 end arch;
