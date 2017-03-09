e592391a2dc53ca63e9baece442594857a1fed97
Gripnook <XxAndreixX@hotmail.com>
2017-03-09 16:53:57 -0500

    Updated testbench to match specified duration

--- src/testbench.tcl
+++ src/testbench.tcl
@@ -45,7 +45,7 @@ force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1 ns
 AddWaves
 
 ;# Run
-run 1us
+run 10us
 
 ;# Save the memory and register file to files
 mem save -outfile memory.txt -format bin -wordsperline 1 -noaddress /testbench/dut/data_cache/ram_block
