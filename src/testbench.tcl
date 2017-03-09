00738a7237d59f870e99b6785f10fb22f0e5eb07
Gripnook <XxAndreixX@hotmail.com>
2017-03-09 17:47:33 -0500

    Cleaned up tcl testbench

--- src/testbench.tcl
+++ src/testbench.tcl
@@ -1,17 +1,20 @@
 proc AddWaves {} {
-    ;#Add waves we're interested in to the Wave window
+    # Add waves we're interested in to the Wave window
     add wave -position end sim:/testbench/clock
     add wave -position end sim:/testbench/reset
+    # Processor state
     add wave -position end sim:/testbench/dut/pc
     add wave -position end sim:/testbench/dut/if_npc
     add wave -position end sim:/testbench/dut/id_opcode
     add wave -position end sim:/testbench/dut/id_funct
-    add wave -position end sim:/testbench/dut/id_target
     add wave -position end sim:/testbench/dut/id_rs_addr
     add wave -position end sim:/testbench/dut/id_rt_addr
     add wave -position end sim:/testbench/dut/id_immediate
     add wave -position end sim:/testbench/dut/id_branch_taken
-    add wave -position end sim:/testbench/dut/id_branch_target
+    # Stall signals
+    add wave -position end sim:/testbench/dut/if_waitrequest
+    add wave -position end sim:/testbench/dut/mem_waitrequest
+    add wave -position end sim:/testbench/dut/data_hazard_stall
     # Performance counters
     add wave -position end -radix 10 sim:/testbench/dut/memory_access_stall_count
     add wave -position end -radix 10 sim:/testbench/dut/data_hazard_stall_count
@@ -20,7 +23,7 @@ proc AddWaves {} {
 
 vlib work
 
-;# Compile components
+# Compile components
 vcom -2008 MIPS_encoding.vhd
 vcom -2008 memory.vhd
 vcom -2008 registers.vhd
@@ -29,24 +32,24 @@ vcom -2008 hazard_detector.vhd
 vcom -2008 processor.vhd
 vcom -2008 testbench.vhd
 
-;# Start simulation
+# Start simulation
 vsim -t ps testbench
 
-;# Load program.txt into the instruction memory
+# Load program into the instruction memory
 mem load -infile program.txt -format bin -filldata 0 /testbench/dut/instruction_cache/ram_block
 
-;# Initialize data memory with zeros
+# Initialize data memory with zeros
 mem load -filldata 0 /testbench/dut/data_cache/ram_block
 
-;# Generate a clock with 1 ns period
+# Generate a clock with 1 ns period
 force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1 ns
 
-;# Add the waves
+# Add the waves
 AddWaves
 
-;# Run
+# Run
 run 10us
 
-;# Save the memory and register file to files
+# Save the memory and register file to files
 mem save -outfile memory.txt -format bin -wordsperline 1 -noaddress /testbench/dut/data_cache/ram_block
 mem save -outfile register_file.txt -format bin -wordsperline 1 -noaddress /testbench/dut/register_file/registers
