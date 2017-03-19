6e57070ea9a5eaa33931e760d4715271300e7a69
vladglv <vladglv@users.noreply.github.com>
2017-03-19 16:29:50 -0400

    Added a bitcnt program

---
+++ src/test-programs/bitcnt/bitcnt.asm
@@ -0,0 +1,47 @@
+# bitcnt.asm
+
+# $1 => x
+# $2 => mask
+# $3 => cnt (result)
+# $4 => i
+# $5 => 32
+# $6 => i < 32
+# $7 => x & mask
+# $8 => 1
+# $9 => x & mask > 0
+# $20 => data section address
+
+addi $20, $0, 2000          # Initializing the beginning of Data Section address in memory
+addi $5, $0, 32             # Constant 32
+addi $8, $0, 1              # Constant 1
+
+# Prepare arguments
+addi $1, $0, 96             # Input x = 96
+jal bitcnt                  # Jump to function
+sw $3, 0($20)               # Save the result in memory
+
+addi $10, $3, 0             # Save result to reg 10
+
+addi $1, $0, 7              # Input x = 7
+jal bitcnt                  # Jump to function
+sw $3, 4($20)               # Save the result in memory
+
+addi $11, $3, 0             # Save result to reg 11
+
+EoP:    beq  $0, $0, EoP    # End of program (infinite loop)
+
+# Function body
+bitcnt:    addi $2, $0, 1   # mask = 1
+           add  $3, $0, $0  # cnt = 0
+           add  $4, $0, $0  # i = 0
+
+loop:   slt  $6, $4, $5     # i < 32
+        bne  $6, $8, quit   # if(!(i < 32))
+        and  $7, $1, $2     # x & mask
+        slt  $9, $0, $7     # (x & mask) > 0
+        bne  $9, $8, b1     # if (!((x & mask) > 0))
+        addi $3, $3, 1      # cnt = cnt + 1
+b1:     sll  $2, $2, 1      # mask = mask << 1
+        addi $4, $4, 1      # i = i + 1
+        j loop              # repeat the loop
+quit:   jr $31  
